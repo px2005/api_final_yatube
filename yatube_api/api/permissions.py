61d23666cd1e55c3bb8604e6f5c35593081e7ebf
Александр Горыничев <px2005@yandex.ru>
2022-05-12 10:16:36 +0300

    API_FINAL v.0.03

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,13 +4,11 @@ from rest_framework import permissions
 class AuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
-        )
+                )
 
     def has_object_permission(self, request, view, obj):
-        return (
-                obj.author == request.user
+        return (obj.author == request.user
                 or request.method in permissions.SAFE_METHODS
-        )
+                )
