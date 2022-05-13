925ed996d1ed60aa9f2e10de578f8f15fd1302f4
Александр Горыничев <px2005@yandex.ru>
2022-05-13 10:02:05 +0300

    API_FINAL v.0.04

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -41,7 +41,7 @@ class FollowSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Follow
-        fields = '__all__'
+        fields = ('user', 'following',)
         validators = (
             UniqueTogetherValidator(
                 queryset=Follow.objects.all(),
